7d8de616743fc4c1d099a6be881ad24fd921cb45
d.manton@victorianplumbing.onmicrosoft.com <dave.manton@victorianplumbing.co.uk>
2021-08-24 09:48:48 +0100

    Not sure

--- Domain/Product.cs
+++ Domain/Product.cs
@@ -1,38 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Domain
 {
-    public class ProductStatus
-    {
-        private HashSet<Product> _products;
-
-        public ProductStatus(string sku, decimal price, int quantity)
-        {
-            _products = new HashSet<Product>();
-
-            Sku = sku;
-            Price = price;
-
-            for (var i = 0; i < quantity; i++)
-                _products.Add(Product.CreateProduct(sku));
-        }
-
-        public string Sku { get; private init; }
-        public decimal Price { get; private init; }
-        public bool IsAvailable => _products.Any();
-        public IReadOnlyCollection<Product> Products => _products;
-
-        public Product GetProduct()
-        {
-            var product = _products.First();
-            _products.Remove(product);
-
-            return product;
-        }
-    }
-
     public class Product
     {
         private Product(string sku, string name)
